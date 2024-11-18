09ab702feb471fe2f12234e05393878983f1570c
acarreterob01 <acarreterob01@iescastelar.com>
2024-11-18 09:54:08 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Scrip de prueba
+REM Autor: Toni Carretero Barroso
+REM Fecha: 18-11-2024
+
+mkdir %userprofile%\asir %userprofile%\daw %userprofile%\ceti %userprofile%\dam %userprofile%\smr
+
+mkdir %userprofile%\asir\docs mkdir %userprofile%\asir\pract mkdir %userprofile%\asir\exam
+mkdir %userprofile%\daw\docs mkdir %userprofile%\daw\pract mkdir %userprofile%\daw\exam
+mkdir %userprofile%\ceti\docs mkdir %userprofile%\ceti\pract mkdir %userprofile%\ceti\exam
+mkdir %userprofile%\dam\docs mkdir %userprofile%\dam\pract mkdir %userprofile%\dam\exam
+mkdir %userprofile%\smr\docs mkdir %userprofile%\smr\pract mkdir %userprofile%\smr\exam
